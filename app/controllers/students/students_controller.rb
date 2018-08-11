a6c960b12ed92b8f230df9523e46e8894b603644
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-11 14:53:46 +0000

    automatically backed up by learn

--- app/controllers/students/students_controller.rb
+++ app/controllers/students/students_controller.rb
@@ -1,5 +1,18 @@
-class StudentsController < ApplicationController 
-  def new 
+class StudentsController < ApplicationController
+
+  def new
     @student = Student.new
+  end
+
+  def create 
+    
+  end 
+
+  def edit 
+
+  end 
+
+  def update 
+
   end 
 end 
