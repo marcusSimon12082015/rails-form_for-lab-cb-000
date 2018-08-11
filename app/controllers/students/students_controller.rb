0bda7e5ef9a722f5ca8c1bd30c475dbd20e26c26
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-11 15:05:33 +0000

    automatically backed up by learn

--- app/controllers/students/students_controller.rb
+++ app/controllers/students/students_controller.rb
@@ -6,7 +6,7 @@ class StudentsController < ApplicationController
 
   def create
     @student.create(student_params(:first_name,:last_name))
-    redirect_to student_path(@student)  
+    redirect_to student_path(@student)
   end
 
   def edit
@@ -17,11 +17,11 @@ class StudentsController < ApplicationController
 
   end
 
-  def show 
+  def show
     @student = Student.find(params[:id])
-  end 
-  
+  end
+
   def student_params(*args)
     params.require(:student).permit(*args)
-  end 
+  end
 end
